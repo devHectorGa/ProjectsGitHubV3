1019e49ec547fa440e7345f5cd44784994931d1a
Dimas Mendoza <damendoza98@gmail.com>
2020-09-15 16:39:21 -0500

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -19,10 +19,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        {/* <h2>It is {this.peticion()}.</h2> */}
-      </div>
+      {this.state.data.map({repositorio => repositorio })}
     );
   }
 }
