3ac7ca4edefcb941b27e9b0ed5baeb29dc958fc2
Dimas Mendoza <damendoza98@gmail.com>
2020-09-16 18:27:14 -0500

    Se lee un campo de texto

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from "react";
 // import css from estilos.css
-import './App.css';
-const ruta = 'https://api.github.com/users/';
-const user = 'dimasmendoza';
+import "./App.css";
+const ruta = "https://api.github.com/users/";
+// const user = "dimasmendoza";
 class App extends Component {
   state = {
     data: [],
+    value: "",
   };
   peticion = () => {
-    fetch(ruta + user + '/repos')
+    fetch(ruta + this.state.value + "/repos")
       .then((res1) => res1.json())
       .then((json1) => {
         this.setState({ data: json1 });
       });
   };
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   componentDidMount() {
     this.peticion();
   }
   render() {
     const { data } = this.state;
+    const usr = (this.handleChange = this.handleChange.bind(this));
+    this.handleSubmit = this.handleSubmit.bind(this);
     return (
-      <Fragment>
-        {data.map((repositories) => (
-          <p>{repositories.full_name}</p>
-        ))}
-      </Fragment>
+      <section>
+        <header className="header">
+          <h1 className="header_Title">Proyectos GitHub</h1>
+        </header>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
     );
   }
 }
